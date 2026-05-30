999484b2c8ac0fdd611dabe16e748cff6739f5dc
mjtcommerce7-ctrl <mjtcommerce7@gmail.com>
2026-05-30 20:34:02 +0200

    Update product.js

--- netlify/functions/product.js
+++ netlify/functions/product.js
@@ -11,24 +11,6 @@ exports.handler = async function(event) {
 
   const product = body.product;
 
-  // Fix variants: als er meerdere varianten zijn, voeg een option toe
-  if (product.variants && product.variants.length > 1) {
-    product.options = [{ name: body.optionName || 'Formaat' }];
-    product.variants = product.variants.map(v => ({
-      ...v,
-      option1: v.title
-    }));
-  } else if (product.variants && product.variants.length === 1) {
-    // 1 variant: verwijder de title zodat Shopify default gebruikt
-    const v = product.variants[0];
-    product.variants = [{
-      price: v.price || '0.00',
-      compare_at_price: v.compare_at_price || null,
-      inventory_management: v.inventory_management || null
-    }];
-  }
-
-  // Fix metafields: verwijder lege waarden
   if (product.metafields) {
     product.metafields = product.metafields.filter(m => m.value && m.value !== '');
   }
