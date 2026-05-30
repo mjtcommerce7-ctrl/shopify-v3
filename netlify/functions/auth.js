32f09b2ed736f3c1f5e0b46586ceb9ee4b825435
mjtcommerce7-ctrl <mjtcommerce7@gmail.com>
2026-05-30 19:16:27 +0200

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,7 +1,7 @@
 exports.handler = async function(event) {
   const CLIENT_ID = process.env.SHOPIFY_CLIENT_ID;
   const STORE = 'nxacmt-ks.myshopify.com';
-  const REDIRECT_URI = 'https://stellular-syrniki-04d7f0.netlify.app/.netlify/functions/auth-callback';
+ const REDIRECT_URI = 'https://perfumora-pp.netlify.app/.netlify/functions/auth-callback';
   const SCOPES = 'write_products,read_products,write_files,read_files';
 
   const authUrl = `https://${STORE}/admin/oauth/authorize?client_id=${CLIENT_ID}&scope=${SCOPES}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
